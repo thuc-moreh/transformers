70d8fe6a5fe5d80e2cb326e9d3eb5181f55060cb
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-10 17:42:44 +0900

    add training logs for language modeling task

---
+++ examples/pytorch/language-modeling/train_clm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Train causal language model
+python run_clm.py \
+    --model_name_or_path openai-gpt \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size 400 \
+    --per_device_eval_batch_size 400 \
+    --do_train \
+    --do_eval \
+    --overwrite_output_dir \
+    --output_dir /tmp/test-clm >training_logs/gpt_moreh_xlarge512gb_bs400.txt
