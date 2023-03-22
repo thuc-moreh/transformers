96c3756aebd77289b98c9b7610d4821c90f712d3
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-22 17:36:20 +0900

    add training logs

--- examples/pytorch/translation/translate.sh
+++ examples/pytorch/translation/translate.sh
@@ -6,8 +6,8 @@ python run_translation.py \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 128 \
+    --per_device_train_batch_size 256 \
     --overwrite_output_dir \
     --num_train_epochs 3 \
     --max_train_samples 10000 \
-    --predict_with_generate > training_logs/facebook-bart-large-mnli_moreh_medium128gb_bs128.text
+    --predict_with_generate > training_logs/facebook-bart-large-mnli_moreh__medium128g_bs256.text
