263d58e9308a2beed7c8309579abf8fcfb12ef1e
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-22 04:43:53 +0000

    add training logs for language modeling task

---
+++ examples/pytorch/language-modeling/train_mlm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Train causal language model
+python run_mlm.py \
+    --model_name_or_path facebook/bart-large\
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size 3 \
+    --per_device_eval_batch_size 3 \
+    --do_train \
+    --do_eval \
+    --overwrite_output_dir \
+    --output_dir /tmp/test-mlm > training_logs/facebook-bart-large_nvidia_bs3.txt
