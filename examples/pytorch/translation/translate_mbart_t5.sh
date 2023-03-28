c2577134bca6bf097c782f3da7029aef74cefb55
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-28 02:39:16 +0000

    add training logs

--- examples/pytorch/translation/translate_mbart_t5.sh
+++ examples/pytorch/translation/translate_mbart_t5.sh
@@ -1,5 +1,5 @@
 python run_translation.py \
-    --model_name_or_path t5-small \
+    --model_name_or_path t5-large \
     --do_train \
     --source_lang en \
     --target_lang ro \
@@ -7,9 +7,9 @@ python run_translation.py \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 200 \
+    --per_device_train_batch_size 20 \
     --overwrite_output_dir \
     --predict_with_generate \
-    --num_train_epochs 3 \
+    --num_train_epochs 1 \
     --max_train_samples 10000 \
-    --predict_with_generate > training_logs/t5-small_bs200.text
+    --predict_with_generate > training_logs/t5-large_bs20.text
