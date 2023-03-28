0277727c6a0b1bbd5922636548b093221dbd8ca3
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-03-28 11:38:01 +0900

    add training logs

--- examples/pytorch/translation/translate_mbart_t5.sh
+++ examples/pytorch/translation/translate_mbart_t5.sh
@@ -1,13 +1,15 @@
 python run_translation.py \
-    --model_name_or_path ft5-small \
+    --model_name_or_path t5-large \
     --do_train \
     --source_lang en \
     --target_lang ro \
+    --source_prefix "translate English to Romanian: " \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 512 \
+    --per_device_train_batch_size 350 \
     --overwrite_output_dir \
+    --predict_with_generate \
     --num_train_epochs 3 \
     --max_train_samples 5000 \
-    --predict_with_generate > training_logs/t5-small_moreh__medium128g_bs512.text
+    --predict_with_generate > training_logs/t5-large_moreh_xLarge512gb_bs350.text
