615111549e064c1dcc2cbd8efd84724e4139f1e2
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-14 11:24:44 +0900

    test all trainer API features/arguments

--- examples/pytorch/language-modeling/train_clm.sh
+++ examples/pytorch/language-modeling/train_clm.sh
@@ -2,12 +2,22 @@
 
 #Train causal language model
 python run_clm.py \
-    --model_name_or_path openai-gpt \
+    --model_name_or_path gpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 400 \
-    --per_device_eval_batch_size 400 \
+    --per_device_train_batch_size 128 \
+    --per_device_eval_batch_size 128 \
     --do_train \
     --do_eval \
+    --do_predict \
     --overwrite_output_dir \
-    --output_dir /tmp/test-clm >training_logs/gpt_moreh_xlarge512gb_bs400.txt
+    --output_dir /tmp/test-clm 
+    # --optim adamw_anyprecision \
+    # --optim_args "momentum_dtype=bf1oat16"
+    
+
+
+
+
+
+
