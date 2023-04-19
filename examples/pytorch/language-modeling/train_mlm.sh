17ebada94dc9c4b3f7f98a1629b56e15ef5b9649
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-19 13:11:18 +0900

    add traning_log

--- examples/pytorch/language-modeling/train_mlm.sh
+++ examples/pytorch/language-modeling/train_mlm.sh
@@ -2,12 +2,12 @@
 
 #Train causal language model
 python run_mlm.py \
-    --model_name_or_path facebook/bart-large\
+    --model_name_or_path albert-large-v2\
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 3 \
-    --per_device_eval_batch_size 3 \
+    --per_device_train_batch_size 80 \
+    --per_device_eval_batch_size 80 \
     --do_train \
     --do_eval \
     --overwrite_output_dir \
-    --output_dir /tmp/test-mlm > training_logs/facebook-bart-large_moreh_medium128g_bs3.txt
+    --output_dir /tmp/test-mlm > training_logs/albert-large-v2_moreh_medium128g_bs80.txt
