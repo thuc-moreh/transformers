330200fe27b9cb7c81da875dbd2c636cd8e8371d
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-19 17:57:09 +0900

    add tl Hel

--- examples/pytorch/translation/translate.sh
+++ examples/pytorch/translation/translate.sh
@@ -1,13 +1,13 @@
 python run_translation.py \
-    --model_name_or_path ft5-small \
+    --model_name_or_path facebook/bart-large-mnli \
     --do_train \
     --source_lang en \
     --target_lang ro \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 512 \
+    --per_device_train_batch_size 64 \
     --overwrite_output_dir \
     --num_train_epochs 3 \
     --max_train_samples 5000 \
-    --predict_with_generate > training_logs/t5-small_moreh__medium128g_bs512.text
+    --predict_with_generate > training_logs/facebook-bart-large-mnli_moreh__medium128g_bs64.text
