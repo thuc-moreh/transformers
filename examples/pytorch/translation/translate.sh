ecb496422402365a69aa9e0fc8e7dfd55a667bc4
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-19 19:04:45 +0900

    add  training log

--- examples/pytorch/translation/translate.sh
+++ examples/pytorch/translation/translate.sh
@@ -1,13 +1,13 @@
 python run_translation.py \
-    --model_name_or_path t5-base \
+    --model_name_or_path t5-large \
     --do_train \
     --source_lang en \
     --target_lang ro \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 20 \
     --overwrite_output_dir \
     --num_train_epochs 3 \
     --max_train_samples 5000 \
-    --predict_with_generate > training_logs/t5-base_moreh__medium128g_bs64.text
+    --predict_with_generate > training_logs/t5-large_moreh__medium128g_bs20.text
