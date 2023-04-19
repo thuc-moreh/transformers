09ebf472f7a9869db3eb022a7761248462c4517b
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-19 18:10:38 +0900

    add training log fb large

--- examples/pytorch/translation/translate.sh
+++ examples/pytorch/translation/translate.sh
@@ -1,13 +1,13 @@
 python run_translation.py \
-    --model_name_or_path facebook/bart-large-mnli \
+    --model_name_or_path t5-small \
     --do_train \
     --source_lang en \
     --target_lang ro \
     --dataset_name wmt16 \
     --dataset_config_name ro-en \
     --output_dir /tmp/tst-translation \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 180 \
     --overwrite_output_dir \
     --num_train_epochs 3 \
     --max_train_samples 5000 \
-    --predict_with_generate > training_logs/facebook-bart-large-mnli_moreh__medium128g_bs64.text
+    --predict_with_generate > training_logs/t5-small_moreh__medium128g_bs180.text
