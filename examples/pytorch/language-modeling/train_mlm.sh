9cbd509167e25db115380b9ae962349115826424
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-20 17:28:10 +0900

    add training log

--- examples/pytorch/language-modeling/train_mlm.sh
+++ examples/pytorch/language-modeling/train_mlm.sh
@@ -2,12 +2,12 @@
 
 #Train causal language model
 python run_mlm.py \
-    --model_name_or_path roberta-base \
+    --model_name_or_path distilbert-base-uncased \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 32 \
-    --per_device_eval_batch_size 32 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
     --do_train \
     --do_eval \
     --overwrite_output_dir \
-    --output_dir /tmp/test-mlm > training_logs/roberta-base_moreh_medium128g_bs32.txt
+    --output_dir /tmp/test-mlm > training_logs/distilbert-base-uncased_moreh_medium128g_bs64.txt
