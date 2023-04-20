b9015e88401674e496f2dabb82988d48b4d8b96b
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-04-20 12:18:05 +0900

    add training logs

--- examples/pytorch/language-modeling/train_clm.sh
+++ examples/pytorch/language-modeling/train_clm.sh
@@ -2,16 +2,16 @@
 
 #Train causal language model
 python run_clm.py \
-    --model_name_or_path gpt2 \
+    --model_name_or_path distilgpt2 \
     --dataset_name wikitext \
     --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 128 \
-    --per_device_eval_batch_size 128 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --do_train \
     --do_eval \
     --do_predict \
     --output_dir /tmp/test-clm \
-    --overwrite_output_dir
+    --overwrite_output_dir > training_logs/distilgpt2_moreh_medium128g_bs8.txt
     
     
 
