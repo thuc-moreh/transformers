2e0bd0063222abb43d7f65ed3c868c8e7c4e4d3b
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-05-17 05:52:39 +0000

    add training scripts

---
+++ examples/pytorch/language-modeling/train_clm_facebook-bart-large.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##    SETTINGS     ## 
+MODEL=facebook/bart-large
+function mydate {
+    date +'%Y-%m-%d_%H:%M:%S'
+}
+## END OF SETTINGS ## 
+
+BATCH_SIZE=3
+
+LOG_FILE=facebook-bart-large_$(mydate)
+# Medium
+# moreh-switch-models -M 2
+python run_clm.py \
+    --model_name_or_path ${MODEL} \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size ${BATCH_SIZE} \
+    --do_train \
+    --output_dir /tmp/test-clm \
+    --overwrite_output_dir > training_logs/${LOG_FILE} \
+    --seed 1337 
+python upload_s3.py training_logs/${LOG_FILE}
