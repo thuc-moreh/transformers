93f3e6ae561e52caef001430dba34c2deb70d4dd
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-05-22 04:38:19 +0000

    save file train_plm.sh

---
+++ examples/pytorch/language-modeling/train_plm.sh
@@ -0,0 +1,10 @@
+python run_plm.py \
+    --model_name_or_path=xlnet-base-cased \
+    --dataset_name wikitext \
+    --dataset_config_name wikitext-2-raw-v1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --do_train \
+    --do_eval \
+    --output_dir /tmp/test-plm \
+    --past_index 
