407bebde66703fb1026805e7ab4d50a39569543f
thuc-moreh <hoang.thuc@moreh.com.vn>
2023-05-29 01:33:28 +0900

    train Hubert and XLSR-W2V

---
+++ examples/pytorch/audio-classification/train_XLSR-Wav2Vec2.sh
@@ -0,0 +1,25 @@
+python run_audio_classification.py \
+    --model_name_or_path facebook/wav2vec2-large-xlsr-53 \
+    --dataset_name superb \
+    --dataset_config_name ks \
+    --output_dir audio_classification_output \
+    --overwrite_output_dir \
+    --remove_unused_columns False \
+    --do_train \
+    --fp16 \
+    --learning_rate 3e-5 \
+    --max_length_seconds 1 \
+    --attention_mask False \
+    --warmup_ratio 0.1 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 32 \
+    --gradient_accumulation_steps 4 \
+    --dataloader_num_workers 4 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --metric_for_best_model accuracy \
+    --save_total_limit 3 \
+    --seed 0 \
